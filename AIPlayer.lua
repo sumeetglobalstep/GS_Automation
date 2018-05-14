57e91e1727bbee99b777362e2fe721c1b9e1fd54
GS\GS1800 <sumeet.bansal@globalstep.com>
2018-05-14 20:18:20 +0530

    Automated till Battle 3.

--- AIPlayer.lua
+++ AIPlayer.lua
@@ -2,84 +2,96 @@ require "Media/GameData/Interactions/Util"
 require "Media/GameData/Interactions/GSAutomation/StateActions"
 require "Media/GameData/Interactions/GSAutomation/InteractionAPI"
 
-previousGameState = ""
+
+function CriticalPopUp()
+	if FindElementByPath("*.ui.textEntry.doneButton") then
+		FindAndClickByPath("*.ui.textEntry.doneButton")
+		return true
+	end
+	if FindElementByPath("*.ui.critical.doneButton") then
+		FindAndClickByPath(".ui.critical.doneButton")
+		return true
+	end
+	
+	return false
+end
 
 function PopUpActive()
 	if FindElement("REGISTRATION_CONTINUE", "", "") then
-    pre_click_function()
-    FindAndClickByPath("*.Registration.*")
-    FindAndClick("REGISTRATION_CONTINUE", "", "")
-    return true
-  elseif FindElementByPath("*.doneButton") then
-    pre_click_function()
-    FindAndClickByPath("*.doneButton")
-    return true
-  elseif FindElementByPath("*.breadcrumbs.*.PopupXXL.*") then
-    pre_click_function()
-    FindAndClick("BTN2_CLICKED", "", "")
-    return true
-  elseif FindElementByPath("*.Rewards.popupHolder.*.btn1") then
-    pre_click_function()
-    FindAndClickByPath("*.Rewards.popupHolder.*.btn1")
-    return true
-  elseif FindElementByPath("*.eventSummaryPopup.closeButton") then
-    pre_click_function()
-    FindAndClickByPath("*.eventSummaryPopup.closeButton")
-    return true
-  elseif FindElementByPath("*.SoftPromptRating.*") then
-    pre_click_function()
-    if FindElementByPath("*.SoftPromptRating.closeBtn.*") then
-      FindAndClickByPath("*.SoftPromptRating.closeBtn.*")
-    else
-      FindAndClick("CLOSE", "", "")
-    end
-  elseif FindElement("SERVER_RESYNC", "", "") then
-    pre_click_function()
-    FindAndClick("SERVER_RESYNC", "", "")
-    return true
-  elseif FindElement("SEND_TO_TITLE", "", "") then
-    pre_click_function()
-    FindAndClick("SEND_TO_TITLE", "", "")
-    return true
-  end
-
-  return false
+		FindAndClickByPath("*.Registration.*")
+		FindAndClick("REGISTRATION_CONTINUE", "", "")
+		return true
+	elseif FindElementByPath("*.outOfResourcesItem.tapArea") then
+		FindAndClickByPath("*.outOfResourcesItem.tapArea")
+		return true
+	elseif FindElementByPath("*.doneButton") then   --Done/Ok Button
+		FindAndClickByPath("*.doneButton")
+		return true
+	elseif FindElementByPath("*.upgradeButton") then -- Skip/Upgrade Button
+		FindAndClickByPath("*.upgradeButton")
+		return true
+	elseif FindElementByPath("*.breadcrumbs.*.PopupXXL.*") then
+		FindAndClick("BTN2_CLICKED", "", "")
+		return true
+	elseif FindElementByPath("*.Rewards.popupHolder.*.btn1") then
+		FindAndClickByPath("*.Rewards.popupHolder.*.btn1")
+		return true
+	elseif FindElementByPath("*.eventSummaryPopup.closeButton") then
+		FindAndClickByPath("*.eventSummaryPopup.closeButton")
+		return true
+	elseif FindElementByPath("*.SoftPromptRating.*") then
+		if FindElementByPath("*.SoftPromptRating.closeBtn.*") then
+			FindAndClickByPath("*.SoftPromptRating.closeBtn.*")
+		else
+			FindAndClick("CLOSE", "", "")
+		end
+	elseif FindElement("SERVER_RESYNC", "", "") then
+		FindAndClick("SERVER_RESYNC", "", "")
+		return true
+	elseif FindElement("SEND_TO_TITLE", "", "") then
+		FindAndClick("SEND_TO_TITLE", "", "")
+		return true
+	elseif (GetCurrentState() ~= "DYNAMIC_DEPLOYMENT") then
+		if FindElementByPath("*.ui.talkingHead.animationContainer.*") then
+			WaitForUI()
+			GenerateClick(0.5,0.5)
+			return true
+		end
+	end
+	
+	return false
 end
 
 function StateHandled()
 	print ("StateHandled called")
 	if	ControlGameState()  then
 		return true
-	else
-		return false
 	end
+	
+	return false
 end
 
 function AIControl()
-
-	if PopUpActive() then
-		print("Will Dismiss any PopUp")
+	if CriticalPopUp() then
+		print("Will Dismiss any CriticalPopUp")
+		return true
 	elseif GuideActive() then
 		print("Will Follow Guide")
 		FollowGuide()
+		return true
+	elseif PopUpActive() then
+		print("Will Dismiss any PopUp")
+		return true
 	elseif StateHandled() then
-	--previousGameState ~= GetCurrentState() then
-		--previousGameState = GetCurrentState()
-	--	if StateHandled() then
-		Wait(5.0)
-	return true
-	
-	--else
-	--		return false
-		--end
+		return true
 	else
 		print("Script Waiting To Take Action")
-		Wait(5.0)
 	end
 	
-	Wait(2.0)
+	return false
 end
 
 while not ExitGame() do
 	AIControl()
+	Wait(2.0)
 end
--- StateActions.lua
+++ StateActions.lua
@@ -1,42 +1,69 @@
 local path = require "Media/GameData/Interactions/GSAutomation/GlobalVars"
 
 GameState = {}
+armyUnitsX = {0.2 , 0.36 , 0.52 , 0.77}
+armyUnitsY = 0.6
+
+targetX = 0.5
+targetY = 0.3
+
+GameState["RESULTS"] = function()
+	print("Inside Results state")
+	if FindElementByPath("*.ui.rewardsScreen.continueButton") then
+		FindAndClickByPath("*.ui.rewardsScreen.continueButton")
+	end
+	if FindElementByPath("*.ui.xpScreen.continueButton") then
+		FindAndClickByPath("*.ui.xpScreen.continueButton")
+	end
+end
+
+GameState["REWARDS"] = function()
+	print("Inside REWARDS state")
+	if FindElementByPath("*.ui.okButton") then
+		FindAndClickByPath("*.ui.okButton")
+	end
+	
+	return false
+end
+
 GameState["BATTLEGAME"] = function()
 	print("BATTLEGAME state called")
 	if FindElementByPath(path.cinematicSkipButton) then
 		print("Skip Button Found")
 		FindAndClickByPath(path.cinematicSkipButton)
 		return true
-	else
-		return false
 	end
+	
+	return false
 end
 
 GameState["DYNAMIC_DEPLOYMENT"] = function()
 	print("DYNAMIC_DEPLOYMENT state called")
 	local actionFound = false
 	
-	if FindElementByPath("*.autoOrder") then
+	if FindElementByPath("*.autoOrders.Button") then
 		print("AutoOrder Found");
+		FindAndClickByPath("*.autoOrders.Button")
 		actionFound = true
 	end	
 	if FindElementByPath("*.ui.multiSelectButton.button") then
 		FindAndClickByPath("*.ui.multiSelectButton.button")
 		WaitForUI()
-		GenerateClick(0.5,0.2)
+		GenerateClick(0.5,0.3)
 		actionFound = true
 	end
-	if FindElementByPath("*.TutorialUnitCommand1.*") then
-		print("TutorialUnitCommand1 Found")
-		 x, y = GetElementCentre("*.TutorialUnitCommand1.*")
-		GenerateClick(x+0.15,y+0.05)
-		 WaitForUI ()
-		-- if FindElementByPath("*.TutorialUnitCommand2.*") then
-		-- print("TutorialUnitCommand2 Found")
-		 -- x, y = GetElementCentre("*.TutorialUnitCommand2.*")
-		-- GenerateClick(x+0.15,y+0.05)
+	if actionFound == false then -- For Initial Battle1 and Battle2
+
+		-- event AI_CONTROL doesn't work, Passing HardCode values
+		
+		for i,armyCoordinateX in ipairs(armyUnitsX) do
+			print("armyCoordinateX",armyCoordinateX)
+			GenerateClick(armyCoordinateX,armyUnitsY)
+			WaitForUI()
+			GenerateClick(targetX,targetY)
+		end
 		actionFound = true
-		--end
+		WaitForUI()
 	end	
 	if actionFound == true then
 		if FindElementByPath("*ui.okButton.button") then
@@ -51,9 +78,9 @@ function ControlGameState()
 	print("ControlGameState called")
 	if GameState[GetCurrentState()] then
 		GameState[GetCurrentState()]()
-	print ("CurrentGameState",GetCurrentState())
+		print ("CurrentGameState",GetCurrentState())
 		return true
-	else
-		return false
 	end
+	
+	return false
 end
